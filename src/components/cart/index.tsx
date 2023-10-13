22b3da9ca7b85e00ecaca7e413d25b709e88989f
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-13 18:54:05 -0300

    refactor: rename files

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { Card, CardContent } from "@mui/material";
 import { CartOrder, CartOrderMobile } from "./style";
+import { useCart } from "../../hooks/useCart";
 
-export const Order: React.FC = () => {
+export const Cart: React.FC = () => {
+  const { handleFetchCart } = useCart();
+
+  useEffect(() => {
+    handleFetchCart();
+  }, []);
   return (
     <Card>
       <CardContent>
