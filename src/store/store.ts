2aae2061960823f3857c860c4a62372cae32a3b4
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-13 18:55:29 -0300

    feat: create cart store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import productsReducer from "./products/reducers";
+import { cartReducer } from "./cart/reducer";
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
