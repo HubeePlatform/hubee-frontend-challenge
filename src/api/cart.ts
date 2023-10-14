8b10a9dc979dcc8330fd125c856ffbd6a313ae1c
RafaelSilva-si <rafael.silva@digi.ag>
2023-10-14 09:23:40 -0300

    feat: create api for cart

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,2 +1,14 @@
 export const getCarts = () =>
   fetch(`http://localhost:3000/carts?opened=${true}`);
+
+export const getCartById = (id: number) =>
+  fetch(`http://localhost:3000/carts/${id}`);
+
+export const updateCart = (updatedData: any) =>
+  fetch(`http://localhost:3000/carts/${updatedData.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedData),
+  });
